c971da68f195f4862a72b519c6c3fb07d5e010e1
hxm <huangxiaoman@gmail.com>
2021-05-23 21:56:21 +0800

    init

--- explore/main.go
+++ explore/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"crypto/ecdsa"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/p2p/discover/v4wire"
 	"github.com/ethereum/go-ethereum/p2p/enode"
+	"github.com/ethereum/go-ethereum/p2p/enr"
 	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/rlp"
+	log "github.com/sirupsen/logrus"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -14,25 +19,64 @@ type LocalNode struct {
 	key *ecdsa.PrivateKey
 }
 
+func init()  {
+	log.SetLevel(log.TraceLevel)
+}
+
 func main() {
-	/*key, err := crypto.GenerateKey()
+	var sigs = make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	defer signal.Stop(sigs)
+
+
+	key, err := crypto.GenerateKey()
 	if err != nil {
 		panic("couldn't generate key: " + err.Error())
 	}
 
-	ln := &LocalNode{key: key}*/
+	//ln := &LocalNode{key: key}
 
 	bn:= enode.MustParse(params.GoerliBootnodes[0])
 
-}
 
-func makePing(toaddr *net.UDPAddr) *v4wire.Ping {
-	seq, _ := rlp.EncodeToBytes(t.localNode.Node().Seq())
-	return &v4wire.Ping{
+	from:= v4wire.Endpoint{IP: net.IP(enr.IPv4{127, 0, 0, 1}), UDP: 30303}
+	to := v4wire.Endpoint{IP: bn.IP(), UDP: uint16(bn.UDP())}
+	expiration:= 5*time.Second
+
+	ping := &v4wire.Ping{
 		Version:    4,
-		From:       t.ourEndpoint(),
-		To:         v4wire.NewEndpoint(toaddr, 0),
+		From:       from,
+		To:         to,
 		Expiration: uint64(time.Now().Add(expiration).Unix()),
-		Rest:       []rlp.RawValue{seq},
 	}
+
+	packet, _, err :=v4wire.Encode(key, ping)
+	if err!=nil {
+		panic(err)
+	}
+
+
+	udpConn, err :=net.DialUDP("udp", nil, &net.UDPAddr{IP: bn.IP(), Port: bn.UDP()})
+	if err!=nil {
+		panic(err)
+	}
+
+	log.Traceln("send ping")
+	_, err = udpConn.Write(packet)
+	if err!=nil {
+		panic(err)
+	}
+
+	var buf [512]byte
+	log.Traceln("read")
+	n, err :=udpConn.Read(buf[:])
+	if err!=nil {
+		log.Error(err)
+	}
+	//v4wire.Decode(buf[:n])
+	log.Infof("read %d", n)
+
+	udpConn.Close()
+
+	<- sigs
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module eth
 
 go 1.16
 
-require github.com/ethereum/go-ethereum v1.10.3
+require (
+	github.com/ethereum/go-ethereum v1.10.3
+	github.com/sirupsen/logrus v1.2.0
+)
